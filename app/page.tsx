f6d61b2442ea73e77361756ab80601779f92b927
valentimcanejo <valentim.canejo@hotmail.com>
2022-11-19 02:05:00 -0300

    fix: corrige bugs do storybook.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,15 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 export default function HomePage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/tasks");
+  }, [router]);
+
   return (
     <div>
       <h1>Home</h1>
